72e53cfd7850718f9f3c194e549c4641389dcee1
Jonah Luo <wander.shaodw.cat@gmail.com>
2020-12-12 00:55:00 -0500

    setting up the security and inteceptor in backend, further test is needed

---
+++ backend/src/main/java/com/worstbuy/service/JwtUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.worstbuy.service;
+
+import com.worstbuy.dao.UserDAO;
+import com.worstbuy.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // TODO: to update the User object creation
+        User user = userDAO.get(00);
+//        UserEntity user = repo.findByUsername(username);
+        if(user!=null){
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUserName(), user.getPassword(), new ArrayList<>());
+        }else{
+            throw new UsernameNotFoundException("User not found with username: "+ username);
+        }
+    }
+}
